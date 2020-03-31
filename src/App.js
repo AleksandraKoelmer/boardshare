a417ae4227cd41b07feb9be5177862a06eab2080
A.Koelmer <aleksandra.koelmer@gmail.com>
2020-03-31 21:03:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+
+
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import SignUp from './components/auth/SignUp';
+import SignIn from './components/auth/SignIn';
+import EventDetails from './components/events/EventDetails';
+import CreateEvent from './components/events/CreateEvent';
+import Sidebar from './components/layout/Sidebar';
+import UserProfile from './components/user/UserProfile';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Sidebar />
+        <Switch>
+        <Route exact path='/' component={Dashboard} />
+        <Route path ='/signup' component={SignUp} />
+        <Route path ='/signin' component={SignIn} />
+        <Route path ='/event/:id' component={EventDetails} />
+        <Route path ='/create' component={CreateEvent} />
+        <Route path = '/user/:id' component={UserProfile} />
+      
+
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
